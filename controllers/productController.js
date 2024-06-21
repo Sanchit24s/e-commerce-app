075476b495ab398858f96576f5df728ea5232ece
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-06-21 17:50:57 +0530

    Add route and controller of create product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const productModel = require("../models/productModel");
+const getDataUri = require("../utils/features");
+const cloudinary = require('cloudinary');
 
 const getAllProductsController = async (req, res) => {
     try {
@@ -50,4 +52,47 @@ const getSingleProductController = async (req, res) => {
     }
 };
 
-module.exports = { getAllProductsController, getSingleProductController };
+const createProductController = async (req, res) => {
+    try {
+        const { name, description, price, category, stock } = req.body;
+
+        if (!name || !description || !price || !stock) {
+            return res.status(500).send({
+                success: false,
+                message: 'Please provide all fields'
+            });
+        }
+
+        if (!req.file) {
+            return res.status(500).send({
+                success: false,
+                message: 'Please provide product images'
+            });
+        }
+
+        const file = getDataUri(req.file);
+        const cdb = await cloudinary.v2.uploader.upload(file.content);
+        const image = {
+            public_id: cdb.public_id,
+            url: cdb.secure_url
+        };
+
+        await productModel.create({
+            name, description, price, category, stock, images: [image]
+        });
+
+        res.status(201).send({
+            success: true,
+            messages: 'Product Created Successfully'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in Create Product API',
+            error
+        });
+    }
+};
+
+module.exports = { getAllProductsController, getSingleProductController, createProductController };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { getAllProductsController, getSingleProductController } = require('../controllers/productController');
+const { getAllProductsController, getSingleProductController, createProductController } = require('../controllers/productController');
+const isAuth = require('../middlewares/authMiddleware');
+const singleUpload = require('../middlewares/multer');
 
 const router = express.Router();
 
 router.get('/get-all', getAllProductsController);
 
 router.get('/:id', getSingleProductController);
+
+router.post('/create', isAuth, singleUpload, createProductController);
 
 module.exports = router;
