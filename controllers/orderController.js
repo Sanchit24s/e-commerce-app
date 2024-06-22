14033a775b8170f58047aefcb1d6dbe9f7075a30
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-06-23 02:22:06 +0530

    Add route and controller for get My Order and single Order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -61,4 +61,64 @@ const createOrderController = async (req, res) => {
     }
 };
 
-module.exports = { createOrderController };
+const getMyOrdersController = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ user: req.user._id });
+
+        if (!orders) {
+            return res.status(404).send({
+                success: false,
+                message: 'Order not found'
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: "your orders data",
+            totalOrder: orders.length,
+            orders
+
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in Get My Order API',
+            error
+        });
+    }
+};
+
+const singleOrderDetailsController = async (req, res) => {
+    try {
+        const order = await orderModel.findById(req.params.id);
+        if (!order) {
+            return res.status(404).send({
+                success: false,
+                message: 'Order Not Found'
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: "your order fetched",
+            order
+        });
+    } catch (error) {
+        console.log(error);
+        // cast error ||  OBJECT ID
+        if (error.name === "CastError") {
+            return res.status(500).send({
+                success: false,
+                message: "Invalid Id",
+            });
+        }
+        res.status(500).send({
+            success: false,
+            message: "Error In Get UPDATE Products API",
+            error,
+        });
+
+    }
+};
+module.exports = { createOrderController, getMyOrdersController, singleOrderDetailsController };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const isAuth = require('../middlewares/authMiddleware');
-const { createOrderController } = require('../controllers/orderController');
+const { createOrderController, getMyOrdersController, singleOrderDetailsController } = require('../controllers/orderController');
 
 const router = express.Router();
 
 router.post('/create', isAuth, createOrderController);
+
+router.get('/my-orders', isAuth, getMyOrdersController);
+
+router.get('/my-orders/:id', isAuth, singleOrderDetailsController);
 
 
 module.exports = router;
